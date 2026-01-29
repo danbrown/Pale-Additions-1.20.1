d6bb2768890d28a63860fc658769cd4b4582585a
ruthless3r <110812652+ruthless3r@users.noreply.github.com>
2026-01-28 22:58:56 -0600

    Initial commit

---
+++ src/main/java/net/ruthless3r/pale_additions/PaleAdditionsClient.java
@@ -0,0 +1,10 @@
+package net.ruthless3r.pale_additions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PaleAdditionsClient  implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/ruthless3r/pale_additions/PaleAdditionsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ruthless3r.pale_additions;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PaleAdditionsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
